a444b1bc88f4d17bdc1dbf22bf90f28f57849e53
greidinger-reis <jp.greidinger@hotmail.com>
2023-01-01 17:34:00 -0300

    2021/01 ok

---
+++ src/2021/01/index.ts
@@ -0,0 +1,60 @@
+import * as Opt from "fp-ts/lib/Option.js";
+import { pipe } from "fp-ts/lib/function.js";
+import { Option } from "fp-ts/lib/Option.js";
+
+function parseInput(input: Option<string>): Option<number[]> {
+    return pipe(
+        input,
+        Opt.map((input) => input.split("\n")),
+        Opt.map((input) => input.map((x) => parseInt(x)))
+    );
+}
+
+export function part1(input: Option<string>): number {
+    return pipe(
+        input,
+        parseInput,
+        calculateDifference,
+        countIncreases,
+        Opt.getOrElse(() => 0)
+    );
+}
+
+export function part2(input: Option<string>): number {
+    return pipe(
+        input,
+        parseInput,
+        toThreeSum,
+        calculateDifference,
+        countIncreases,
+        Opt.getOrElse(() => 0)
+    );
+}
+
+//create an array of sums of each number with it's next two numbers
+function toThreeSum(input: Option<number[]>): Option<number[]> {
+    return pipe(
+        input,
+        Opt.chain((input) =>
+            Opt.fromNullable(
+                input
+                    .map((x, i, arr) => x + arr[i + 1] + arr[i + 2])
+                    .filter((x) => !isNaN(x))
+            )
+        )
+    );
+}
+
+function calculateDifference(input: Option<number[]>): Option<number[]> {
+    return pipe(
+        input,
+        Opt.map((input) => input.map((x, i, arr) => arr[i + 1] - x))
+    );
+}
+
+function countIncreases(input: Option<number[]>): Option<number> {
+    return pipe(
+        input,
+        Opt.map((input) => input.reduce((acc, x) => acc + (x > 0 ? 1 : 0), 0))
+    );
+}
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("Hello World");
+import fs from "node:fs";
+import { tryCatch } from "fp-ts/lib/Option.js";
+import { part2 } from "./2021/01/index.js";
+
+const input = tryCatch(() => fs.readFileSync("src/2021/01/input.txt", "utf-8"));
+const testInput = tryCatch(() =>
+    fs.readFileSync("src/2021/01/input_test.txt", "utf-8")
+);
+// import { part1 } from "./2021/01/index.js";
+// console.log("day 1, part 1 test input\t",part1(testInput));
+// console.log("day 1, part 1 real input\t",part1(input));
+console.log("day 1, part 2 test input:\t", part2(testInput));
+console.log("day 1, part 2 real input:\t", part2(input));
