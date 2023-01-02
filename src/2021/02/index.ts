abb502b4b27308dfb5324d018d339462293597e4
greidinger-reis <jp.greidinger@hotmail.com>
2023-01-02 18:58:39 -0300

    notworking

--- src/2021/02/index.ts
+++ src/2021/02/index.ts
@@ -1,15 +1,25 @@
 import { pipe } from "fp-ts/lib/function.js";
 import { Option } from "fp-ts/lib/Option.js";
 import * as Opt from "fp-ts/lib/Option.js";
+import * as Arr from "fp-ts/lib/Array.js";
 
-export const part1 = (input: Option<string>): number => {
+export function part1(input: Option<string>): number {
     return pipe(
         input,
         parseInput,
         calculatePosition,
         Opt.getOrElse(() => 0)
     );
-};
+}
+
+export function part2(input: Option<string>): number {
+    return pipe(
+        input,
+        parseInput,
+        calculatePositionPt2,
+        Opt.getOrElse(() => 0)
+    );
+}
 
 export type Instruction = {
     readonly _type: "forward" | "depth";
@@ -40,7 +50,7 @@ function calculatePosition(input: Option<Instruction[]>): Option<number> {
         input,
         Opt.map((input) => {
             return {
-                forward: input.reduce(
+                horizontal: input.reduce(
                     (acc, curr) =>
                         curr._type === "forward" ? acc + curr.value : acc,
                     0
@@ -53,8 +63,66 @@ function calculatePosition(input: Option<Instruction[]>): Option<number> {
             };
         }),
         Opt.map((input) => {
-            console.log({ depth: input.depth, forward: input.forward });
-            return input.depth * input.forward;
+            return input.depth * input.horizontal;
         })
+    );
+}
+
+function calculatePositionPt2(input: Option<Instruction[]>): Option<number> {
+    return pipe(
+        input,
+        Opt.map((input) =>
+            input.map((instruction, i, instructions) => {
+                return {
+                    forward:
+                        instruction._type === "forward" ? instruction.value : 0,
+                    aim: pipe(
+                        Arr.findLastIndex(
+                            (ins: Instruction) => ins._type === "depth"
+                        )(instructions.slice(0, i + 1)),
+                        Opt.map((i) => {
+                            return {
+                                i,
+                                value: instructions[i].value,
+                            };
+                        }),
+                        Opt.getOrElse(() => {
+                            return { i: 0, value: 0 };
+                        })
+                    ),
+                };
+            })
+        ),
+        Opt.map((input) => {
+            return input.reduce(
+                (acc, curr) => {
+                    console.log({
+                        forward: curr.forward,
+                        aim: {
+                            value:
+                                curr.aim.i !== acc.aim.i
+                                    ? curr.aim.value + acc.aim.value
+                                    : curr.aim.value,
+                            i: curr.aim.i,
+                        },
+                        horizontal: curr.forward + acc.horizontal,
+                        depth: acc.depth + curr.aim.value * curr.forward,
+                    });
+                    return {
+                        horizontal: curr.forward + acc.horizontal,
+                        aim: {
+                            value:
+                                curr.aim.i !== acc.aim.i
+                                    ? curr.aim.value + acc.aim.value
+                                    : curr.aim.value,
+                            i: curr.aim.i,
+                        },
+                        depth: acc.depth + curr.aim.value * curr.forward,
+                    };
+                },
+                { horizontal: 0, aim: { i: 0, value: 0 }, depth: 0 }
+            );
+        }),
+        Opt.map((input) => input.depth * input.horizontal)
     );
 }
--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,8 @@ import { tryCatch } from "fp-ts/lib/Option.js";
 //     fs.readFileSync("src/2021/01/input_test.txt", "utf-8")
 // );
 // import { part1, part2 } from "./2021/01/index.js";
-// console.log("day 1, part 1 test input\t",part1(testInput));
-// console.log("day 1, part 1 real input\t",part1(input));
+// console.log("day 1, part 1 test input:\t",part1(testInput));
+// console.log("day 1, part 1 real input:\t",part1(input));
 // console.log("day 1, part 2 test input:\t", part2(testInput));
 // console.log("day 1, part 2 real input:\t", part2(input));
 
@@ -15,6 +15,7 @@ const input = tryCatch(() => fs.readFileSync("src/2021/02/input.txt", "utf-8"));
 const testInput = tryCatch(() =>
     fs.readFileSync("src/2021/02/input_test.txt", "utf-8")
 );
-import { part1 } from "./2021/02/index.js";
-console.log("day 2, part 1 test input\t", part1(testInput));
-console.log("day 2, part 1 real input\t", part1(input));
+import { part1, part2 } from "./2021/02/index.js";
+console.log("day 2, part 1 test input:\t", part1(testInput));
+console.log("day 2, part 1 real input:\t", part1(input));
+console.log("day 2, part 2 test input:\t", part2(testInput));
